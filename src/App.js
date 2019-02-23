850bb345512a46c4147111782bf75baa0274a2f1
liulinboyi <814921718@qq.com>
2019-02-23 09:55:22 +0800

    'update'

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './images/logo.svg';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <div>你好 这是一个React Todo-List项目</div>
+      </div>
+    );
+  }
+}
+
+export default App;
